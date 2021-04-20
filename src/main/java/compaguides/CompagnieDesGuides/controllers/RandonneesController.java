cf1ab90798d93b371525521b34d439d09f761b30
PereSalty <davidballard46000@gmail.com>
2021-04-20 14:58:00 +0200

    Randonnees !

--- src/main/java/compaguides/CompagnieDesGuides/controllers/RandonneesController.java
+++ src/main/java/compaguides/CompagnieDesGuides/controllers/RandonneesController.java
@@ -3,9 +3,14 @@ package compaguides.CompagnieDesGuides.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Optional;
+
+import compaguides.CompagniesDesGuides.model.RandonneesModel;
 import compaguides.CompagniesDesGuides.model.RandonneesRepository;
 
 @RestController
@@ -21,5 +26,21 @@ public class RandonneesController {
 		mav.addObject("randonnees",randonneesrepository.findAll());
 		mav.setViewName("randonnees-all");
 		return mav;	
+	}
+	
+	@RequestMapping("/randonnees-delete/{id}")
+	public ModelAndView delete(@PathVariable int id) {
+		randonneesrepository.deleteById(id);
+		return new ModelAndView("redirect:/employes");
+	}
+	@GetMapping("/randonnees/{id}")
+	public ModelAndView get(@PathVariable int id) {
+		//Récuperer l'employé
+		Optional<RandonneesModel> e = randonneesrepository.findById(id);
+	
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("randonnees", e);
+		mav.setViewName("randonnees-get");
+		return mav;
 	}
 }
